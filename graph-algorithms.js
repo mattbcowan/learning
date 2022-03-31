d040014d78f870105538652d0377d0f645c7890b
Matt Cowan <matt@mattbcowan.com>
2022-03-31 18:33:45 -0500

    add graph algorithms

---
+++ graph-algorithms.js
@@ -0,0 +1,49 @@
+// Iterative DFS
+// const depthFirstPrint = (graph, source) => {
+//   const stack = [source];
+
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     console.log(current);
+
+//     for (let neighbor of graph[current]) {
+//       stack.push(neighbor);
+//     }
+//   }
+// };
+
+// Recursive DFS
+// const depthFirstPrint = (graph, source) => {
+//   console.log(source);
+//   for (let neighbor of graph[source]) {
+//     depthFirstPrint(graph, neighbor);
+//   }
+// };
+
+// Iterative BFS
+
+const breadthFirstPrint = (graph, source) => {
+  const queue = [source];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    console.log(current);
+
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
+  }
+};
+
+const graph = {
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+// depthFirstPrint(graph, "a");
+
+breadthFirstPrint(graph, "a");
